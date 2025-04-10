de9f3c6b546d7e225863f52f913acd5ecafa6660
ArrJayBrooks <brooks.243@wright.edu>
2025-04-10 11:34:26 -0400

    Adding file existance verification to for loops

--- pisidescript.sh
+++ pisidescript.sh
@@ -9,19 +9,26 @@ cd /home/teamprojects/Desktop/YoloCode/AllImages/IdentifiedDeathStar
 
 for filename in *.png
 do
-	echo "Running on $filename"
-	python3 /home/teamprojects/Desktop/capstoneAES/AESenc.py $filename /home/teamprojects/Desktop/CompletedFiles/$filename.enc
+	if test -f $filename; then
+		echo "Running on $filename"
+		python3 /home/teamprojects/Desktop/capstoneAES/AESenc.py $filename /home/teamprojects/Desktop/CompletedFiles/$filename.enc
+	fi
 done
 
 for filename in *.jpg
 do
-        echo "Running on $filename"
-        python3 /home/teamprojects/Desktop/capstoneAES/AESenc.py $filename /home/teamprojects/Desktop/CompletedFiles/$filename.enc
+	if test -f $filename; then
+        	echo "Running on $filename"
+        	python3 /home/teamprojects/Desktop/capstoneAES/AESenc.py $filename /home/teamprojects/Desktop/CompletedFiles/$filename.enc
+	fi
 done
 
 echo "Encryptions finished. Generating .md5 files"
 cd /home/teamprojects/Desktop/CompletedFiles
 for filename in *.enc
 do
-	md5sum $filename > $filename.md5
+	if test -f $filename; then
+		echo "Checksumming $filename"
+		md5sum $filename > $filename.md5
+	fi
 done
