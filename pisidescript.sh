74f89d2d034f169c691512cdc5f530fd13d6553b
ArrJayBrooks <brooks.243@wright.edu>
2025-04-10 09:38:13 -0400

    Adding status checks and fixing encryption file name

--- pisidescript.sh
+++ pisidescript.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 source /home/pi/.venv/bin/activate
+echo "Running Combined.py"
 python3 /home/teamprojects/Desktop/YoloCode/Combined.py
 
+echo "Combined.py complete. Changing directories."
 cd /home/teamprojects/Desktop/capstoneAES
 
 for filename in /home/teamprojects/Desktop/YoloCode/AllImages/CroppedTestImages/*.{jpg,jpeg,png}
 do
-    python3 capstoneAES.py /home/teamprojects/Desktop/YoloCode/AllImages/CroppedTestImages/*$filename $filename.enc
-    md5sum $filename.enc > $filename.md5
+	echo "Running on $filename"
+	python3 AESenc.py /home/teamprojects/Desktop/YoloCode/AllImages/CroppedTestImages/*$filename $filename.enc
+	echo "Encryption complete. Generating .md5 file"
+	md5sum $filename.enc > $filename.md5
 done
