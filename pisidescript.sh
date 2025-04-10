4a831134de5c35f5743e828a9410abb059fd7a7d
ArrJayBrooks <brooks.243@wright.edu>
2025-04-10 10:23:07 -0400

    Big changes to loops and folder paths

--- pisidescript.sh
+++ pisidescript.sh
@@ -5,28 +5,23 @@ echo "Running Combined.py"
 python3 /home/teamprojects/Desktop/YoloCode/Combined.py
 
 echo "Combined.py complete. Changing directories."
-cd /home/teamprojects/Desktop/capstoneAES
+cd /home/teamprojects/Desktop/YoloCode/AllImages/IdentifiedDeathStar
 
-for filename in /home/teamprojects/Desktop/YoloCode/AllImages/CroppedTestImages/*.png
+for filename in *.png
 do
 	echo "Running on $filename"
-	python3 AESenc.py $filename $filename.enc
-	echo "Encryption complete. Generating .md5 file"
-	md5sum $filename.enc > $filename.md5
+	python3 /home/teamprojects/Desktop/capstoneAES/AESenc.py $filename /home/teamprojects/Desktop/CompletedFiles/$filename.enc
 done
 
-for filename in /home/teamprojects/Desktop/YoloCode/AllImages/CroppedTestImages/*.jpg
+for filename in *.jpg
 do
         echo "Running on $filename"
-        python3 AESenc.py $filename $filename.enc
-        echo "Encryption complete. Generating .md5 file"
-        md5sum $filename.enc > $filename.md5
+        python3 /home/teamprojects/Desktop/capstoneAES/AESenc.py $filename /home/teamprojects/Desktop/CompletedFiles/$filename.enc
 done
 
-for filename in /home/teamprojects/Desktop/YoloCode/AllImages/CroppedTestImages/*.jpeg
+echo "Encryptions finished. Generating .md5 files"
+cd /home/teamprojects/Desktop/CompletedFiles
+for filename in *.enc
 do
-        echo "Running on $filename"
-        python3 AESenc.py $filename $filename.enc
-        echo "Encryption complete. Generating .md5 file"
-        md5sum $filename.enc > $filename.md5
+	md5sum $filename > $filename.md5
 done
