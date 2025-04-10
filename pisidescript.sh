dd32084648040d3ef5766dd98b04c4e3d660e53b
ArrJayBrooks <brooks.243@wright.edu>
2025-04-10 09:49:13 -0400

    Fixed issue with the filename variable. Separated loop into three loops for each file extension

--- pisidescript.sh
+++ pisidescript.sh
@@ -7,10 +7,26 @@ python3 /home/teamprojects/Desktop/YoloCode/Combined.py
 echo "Combined.py complete. Changing directories."
 cd /home/teamprojects/Desktop/capstoneAES
 
-for filename in /home/teamprojects/Desktop/YoloCode/AllImages/CroppedTestImages/*.{jpg,jpeg,png}
+for filename in /home/teamprojects/Desktop/YoloCode/AllImages/CroppedTestImages/*.png
 do
 	echo "Running on $filename"
-	python3 AESenc.py /home/teamprojects/Desktop/YoloCode/AllImages/CroppedTestImages/*$filename $filename.enc
+	python3 AESenc.py $filename $filename.enc
 	echo "Encryption complete. Generating .md5 file"
 	md5sum $filename.enc > $filename.md5
+done
+
+for filename in /home/teamprojects/Desktop/YoloCode/AllImages/CroppedTestImages/*.jpg
+do
+        echo "Running on $filename"
+        python3 AESenc.py $filename $filename.enc
+        echo "Encryption complete. Generating .md5 file"
+        md5sum $filename.enc > $filename.md5
+done
+
+for filename in /home/teamprojects/Desktop/YoloCode/AllImages/CroppedTestImages/*.jpeg
+do
+        echo "Running on $filename"
+        python3 AESenc.py $filename $filename.enc
+        echo "Encryption complete. Generating .md5 file"
+        md5sum $filename.enc > $filename.md5
 done
