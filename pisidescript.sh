972277fd158f1c0a04e1e3ef543b58e9980dfda4
ArrJayBrooks <brooks.243@wright.edu>
2025-04-10 09:30:07 -0400

    Changed path for images

--- pisidescript.sh
+++ pisidescript.sh
@@ -5,8 +5,8 @@ python3 /home/teamprojects/Desktop/YoloCode/Combined.py
 
 cd /home/teamprojects/Desktop/capstoneAES
 
-for filename in /home/teamprojects/Desktop/YoloCode/*.{jpg,jpeg,png}
+for filename in /home/teamprojects/Desktop/YoloCode/AllImages/CroppedTestImages/*.{jpg,jpeg,png}
 do
-    python3 capstoneAES.py /home/teamprojects/Desktop/YoloCode/$filename $filename.enc
+    python3 capstoneAES.py /home/teamprojects/Desktop/YoloCode/AllImages/CroppedTestImages/*$filename $filename.enc
     md5sum $filename.enc > $filename.md5
 done
