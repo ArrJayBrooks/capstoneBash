b581a15e0cc359e53dbb409604163720acfcb39c
ArrJayBrooks <brooks.243@wright.edu>
2025-04-10 08:34:22 -0400

    Changed pisidescript.sh to work with all extensions

--- pisidescript.sh
+++ pisidescript.sh
@@ -5,7 +5,7 @@ python3 /home/teamprojects/Desktop/YoloCode/Combined.py
 
 cd /home/teamprojects/Desktop/capstoneAES
 
-for filename in /home/teamprojects/Desktop/YoloCode/*.png
+for filename in /home/teamprojects/Desktop/YoloCode/*.{jpg,jpeg,png}
 do
     python3 capstoneAES.py /home/teamprojects/Desktop/YoloCode/$filename $filename.enc
     md5sum $filename.enc > $filename.md5
