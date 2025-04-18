ef9f720f54c5ff110508a211daa470db34dd1374
ArrJayBrooks <brooks.243@wright.edu>
2025-04-18 11:23:08 -0400

    Improving serverside

--- serversidescript.sh
+++ serversidescript.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+cd /home/amnesia/capstone
+
 for filename in *.enc
 do
 	if test -f $filename; then
 		echo "Checksumming $filename"
 		md5sum -c $filename.md5
 		echo "Decrypting $filename"
-		python3 AESdec.py $filename $filename.dec
+		python3 capstoneAES/AESdec.py $filename $filename.png
+		echo "Decryption Finished. Running red.py"
+		python3 red.py
 	fi
 done
